0a1bab6f7332c46584c7a889dc1e8de3e5975156
tinoest <tinoest@gmail.com>
2018-10-05 19:08:38 +0100

    STK500v2 Minified Bootloader
    STK500v2 based boot loader, minified from code created by Peter Fleury
    and refactored heavily.

---
+++ Makefile
@@ -0,0 +1,346 @@
+# ----------------------------------------------------------------------------
+# Makefile to compile and link stk500boot bootloader
+# Author: Martyn Brown
+# based on WinAVR Makefile Template written by Eric B. Weddington, Jörg Wunsch, et al.
+#
+# Adjust F_CPU below to the clock frequency in Mhz of your AVR target
+#
+#----------------------------------------------------------------------------
+# On command line:
+#
+# make all = Make software.
+#
+# make clean = Clean out built project files.
+#
+# make program = Download the hex file to the device, using avrdude.
+#                Please customize the avrdude settings below first!
+#
+# To rebuild project do "make clean" then "make all".
+#----------------------------------------------------------------------------
+
+# Processor frequency.
+#     This will define a symbol, F_CPU, in all source code files equal to the
+#     processor frequency. You can then use this symbol in your source code to
+#     calculate timings. Do NOT tack on a 'UL' at the end, this will be done
+#     automatically to create a 32-bit value in your source code.
+#F_CPU = 16000000
+
+# Bootloader
+# Please adjust if using a different AVR
+# 0x0e00*2=0x1C00 for ATmega8  512 words Boot Size
+# 0xFC00*2=0x1F800 for ATmega128  1024 words Boot Size
+# 0xF800*2=0x1F000 for ATmega1280
+# 0xF000*2=0x1E000 for ATmega1280
+#BOOTLOADER_ADDRESS = 1E000
+
+# Output format. (can be srec, ihex, binary)
+FORMAT = ihex
+
+# Target file name (without extension).
+TARGET = stk500boot
+
+# List C source files here. (C dependencies are automatically generated.)
+SRC = stk500boot.c
+
+# List Assembler source files here.
+#     Make them always end in a capital .S.  Files ending in a lowercase .s
+#     will not be considered source files but generated files (assembler
+#     output from the compiler), and will be deleted upon "make clean"!
+#     Even though the DOS/Win* filesystem matches both .s and .S the same,
+#     it will preserve the spelling of the filenames, and gcc itself does
+#     care about how the name is spelled on its command-line.
+ASRC =
+
+# Optimization level, can be [0, 1, 2, 3, s].
+#     0 = turn off optimization. s = optimize for size.
+#     (Note: 3 is not always the best optimization level. See avr-libc FAQ.)
+OPT = s
+
+# Debugging format.
+#     Native formats for AVR-GCC's -g are dwarf-2 [default] or stabs.
+#     AVR Studio 4.10 requires dwarf-2.
+#     AVR [Extended] COFF format requires stabs, plus an avr-objcopy run.
+DEBUG = dwarf-2
+
+# List any extra directories to look for include files here.
+#     Each directory must be seperated by a space.
+#     Use forward slashes for directory separators.
+#     For a directory that has spaces, enclose it in quotes.
+EXTRAINCDIRS =
+
+# Compiler flag to set the C Standard level.
+#     c89   = "ANSI" C
+#     gnu89 = c89 plus GCC extensions
+#     c99   = ISO C99 standard (not yet fully implemented)
+#     gnu99 = c99 plus GCC extensions
+CSTANDARD = -std=gnu99
+
+# Place -D or -U options here
+CDEFS = -DF_CPU=$(F_CPU)UL
+
+# Place -I options here
+CINCS =
+
+#---------------- Compiler Options ----------------
+#  -g*:          generate debugging information
+#  -O*:          optimization level
+#  -f...:        tuning, see GCC manual and avr-libc documentation
+#  -Wall...:     warning level
+#  -Wa,...:      tell GCC to pass this to the assembler.
+#    -adhlns...: create assembler listing
+CFLAGS = -g$(DEBUG)
+CFLAGS += $(CDEFS) $(CINCS)
+CFLAGS += -O$(OPT)
+CFLAGS += -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums -fno-jump-tables
+CFLAGS += -Wall -Wstrict-prototypes
+CFLAGS += -Wa,-adhlns=$(<:.c=.lst)
+CFLAGS += $(patsubst %,-I%,$(EXTRAINCDIRS))
+CFLAGS += $(CSTANDARD)
+
+#---------------- Assembler Options ----------------
+#  -Wa,...:   tell GCC to pass this to the assembler.
+#  -ahlms:    create listing
+#  -gstabs:   have the assembler create line number information; note that
+#             for use in COFF files, additional information about filenames
+#             and function names needs to be present in the assembler source
+#             files 
+ASFLAGS = -Wa,-adhlns=$(<:.S=.lst),-gstabs
+
+#---------------- Library Options ----------------
+# Minimalistic printf version
+PRINTF_LIB_MIN = -Wl,-u,vfprintf -lprintf_min
+
+# Floating point printf version (requires MATH_LIB = -lm below)
+PRINTF_LIB_FLOAT = -Wl,-u,vfprintf -lprintf_flt
+
+# If this is left blank, then it will use the Standard printf version.
+PRINTF_LIB =
+#PRINTF_LIB = $(PRINTF_LIB_MIN)
+#PRINTF_LIB = $(PRINTF_LIB_FLOAT)
+
+# Minimalistic scanf version
+SCANF_LIB_MIN = -Wl,-u,vfscanf -lscanf_min
+
+# Floating point + %[ scanf version (requires MATH_LIB = -lm below)
+SCANF_LIB_FLOAT = -Wl,-u,vfscanf -lscanf_flt
+
+# If this is left blank, then it will use the Standard scanf version.
+SCANF_LIB =
+#SCANF_LIB = $(SCANF_LIB_MIN)
+#SCANF_LIB = $(SCANF_LIB_FLOAT)
+
+MATH_LIB = -lm
+
+#---------------- External Memory Options ----------------
+EXTMEMOPTS =
+
+#---------------- Linker Options ----------------
+#  -Wl,...:     tell GCC to pass this to linker.
+#    -Map:      create map file
+#    --cref:    add cross reference to  map file
+LDFLAGS = -Wl,-Map=$(TARGET).map,--cref
+LDFLAGS += $(EXTMEMOPTS)
+LDFLAGS += $(PRINTF_LIB) $(SCANF_LIB) $(MATH_LIB)
+
+#--------------- bootloader linker Options -------
+# BOOTLOADER_ADDRESS (=Start of Boot Loader section
+# in bytes - not words) is defined above.
+#LDFLAGS += -Wl,--section-start=.text=$(BOOTLOADER_ADDRESS) -nostartfiles -nodefaultlibs
+#LDFLAGS += -Wl,--section-start=.text=$(BOOTLOADER_ADDRESS) -nostartfiles
+LDFLAGS += -Wl,--section-start=.text=$(BOOTLOADER_ADDRESS)
+
+#============================================================================
+
+# Define programs and commands.
+SHELL = sh
+CC = avr-gcc
+OBJCOPY = avr-objcopy
+OBJDUMP = avr-objdump
+SIZE = avr-size
+NM = avr-nm
+AVRDUDE = avrdude
+REMOVE = rm -f
+COPY = cp
+WINSHELL = cmd
+
+# Define Messages
+# English
+MSG_ERRORS_NONE = Errors: none
+MSG_BEGIN = -------- begin --------
+MSG_END = --------  end  --------
+MSG_SIZE_BEFORE = Size before:
+MSG_SIZE_AFTER = Size after:
+MSG_COFF = Converting to AVR COFF:
+MSG_EXTENDED_COFF = Converting to AVR Extended COFF:
+MSG_FLASH = Creating load file for Flash:
+MSG_EEPROM = Creating load file for EEPROM:
+MSG_EXTENDED_LISTING = Creating Extended Listing:
+MSG_SYMBOL_TABLE = Creating Symbol Table:
+MSG_LINKING = Linking:
+MSG_COMPILING = Compiling:
+MSG_ASSEMBLING = Assembling:
+MSG_CLEANING = Cleaning project:
+
+# Define all object files.
+OBJ = $(SRC:.c=.o) $(ASRC:.S=.o)
+
+# Define all listing files.
+LST = $(SRC:.c=.lst) $(ASRC:.S=.lst)
+
+
+# Compiler flags to generate dependency files.
+GENDEPFLAGS = -MD -MP -MF .dep/$(@F).d
+
+# Combine all necessary flags and optional flags.
+# Add target processor to flags.
+ALL_CFLAGS = -mmcu=$(MCU) -I. $(CFLAGS) $(GENDEPFLAGS)
+ALL_ASFLAGS = -mmcu=$(MCU) -I. -x assembler-with-cpp $(ASFLAGS)
+
+############################################################
+#	May 25,	2010	<MLS> Adding 1280 support
+mega1280: MCU = atmega1280
+mega1280: F_CPU = 16000000
+mega1280: BOOTLOADER_ADDRESS = 1E000
+mega1280: CFLAGS += -D_MEGA_BOARD_
+mega1280: begin gccversion sizebefore build sizeafter end
+			mv $(TARGET).hex stk500boot_v2_mega1280.hex
+
+############################################################
+#	Sept 21, 2018	<MGB> Adding 1284P Support	
+mega1284p: MCU = atmega1284p
+mega1284p: F_CPU = 16000000
+mega1284p: BOOTLOADER_ADDRESS = 1E000
+mega1284p: CFLAGS += -D_MEGA_BOARD_
+mega1284p: begin gccversion sizebefore build sizeafter end
+			mv $(TARGET).hex stk500boot_v2_mega1284p.hex
+
+
+############################################################
+#	Jul 6,	2010	<MLS> Adding 2560 support
+mega2560:	MCU = atmega2560
+mega2560:	F_CPU = 16000000
+mega2560:	BOOTLOADER_ADDRESS = 3F800 #3E000
+mega2560:	CFLAGS += -D_MEGA_BOARD_
+mega2560:	begin gccversion sizebefore build sizeafter end
+			mv $(TARGET).hex stk500boot_v2_mega2560.hex
+
+# Default target.
+all: begin gccversion sizebefore build sizeafter end
+
+build: elf hex eep lss sym
+#build:  hex eep lss sym
+
+elf: $(TARGET).elf
+hex: $(TARGET).hex
+eep: $(TARGET).eep
+lss: $(TARGET).lss
+sym: $(TARGET).sym
+
+# Eye candy.
+# AVR Studio 3.x does not check make's exit code but relies on
+# the following magic strings to be generated by the compile job.
+begin:
+	@echo
+	@echo $(MSG_BEGIN)
+
+end:
+	@echo $(MSG_END)
+	@echo
+
+# Display size of file.
+HEXSIZE = $(SIZE) --target=$(FORMAT) $(TARGET).hex
+ELFSIZE = $(SIZE) --format=avr --mcu=$(MCU) $(TARGET).elf
+
+sizebefore:
+	@if test -f $(TARGET).elf; then echo; echo $(MSG_SIZE_BEFORE); $(ELFSIZE); \
+	2>/dev/null; echo; fi
+
+sizeafter:
+	@if test -f $(TARGET).elf; then echo; echo $(MSG_SIZE_AFTER); $(ELFSIZE); \
+	2>/dev/null; echo; fi
+
+# Display compiler version information.
+gccversion :
+	@$(CC) --version
+
+# Create final output files (.hex, .eep) from ELF output file.
+%.hex: %.elf
+	@echo
+	@echo $(MSG_FLASH) $@
+	$(OBJCOPY) -O $(FORMAT) -R .eeprom $< $@
+
+%.eep: %.elf
+	@echo
+	@echo $(MSG_EEPROM) $@
+	-$(OBJCOPY) -j .eeprom --set-section-flags=.eeprom="alloc,load" \
+	--change-section-lma .eeprom=0 -O $(FORMAT) $< $@
+
+# Create extended listing file from ELF output file.
+%.lss: %.elf
+	@echo
+	@echo $(MSG_EXTENDED_LISTING) $@
+	$(OBJDUMP) -h -S $< > $@
+
+# Create a symbol table from ELF output file.
+%.sym: %.elf
+	@echo
+	@echo $(MSG_SYMBOL_TABLE) $@
+	$(NM) -n $< > $@
+
+# Link: create ELF output file from object files.
+.SECONDARY : $(TARGET).elf
+.PRECIOUS : $(OBJ)
+%.elf: $(OBJ)
+	@echo
+	@echo $(MSG_LINKING) $@
+	$(CC) $(ALL_CFLAGS) $^ --output $@ $(LDFLAGS)
+
+# Compile: create object files from C source files.
+%.o : %.c
+	@echo
+	@echo $(MSG_COMPILING) $<
+	$(CC) -c $(ALL_CFLAGS) $< -o $@
+
+
+# Compile: create assembler files from C source files.
+%.s : %.c
+	$(CC) -S $(ALL_CFLAGS) $< -o $@
+
+
+# Assemble: create object files from assembler source files.
+%.o : %.S
+	@echo
+	@echo $(MSG_ASSEMBLING) $<
+	$(CC) -c $(ALL_ASFLAGS) $< -o $@
+
+# Create preprocessed source for use in sending a bug report.
+%.i : %.c
+	$(CC) -E -mmcu=$(MCU) -I. $(CFLAGS) $< -o $@
+
+
+# Target: clean project.
+clean: begin clean_list end
+
+clean_list :
+	@echo
+	@echo $(MSG_CLEANING)
+	$(REMOVE) *.hex
+	$(REMOVE) *.eep
+	$(REMOVE) *.cof
+	$(REMOVE) *.elf
+	$(REMOVE) *.map
+	$(REMOVE) *.sym
+	$(REMOVE) *.lss
+	$(REMOVE) $(OBJ)
+	$(REMOVE) $(LST)
+	$(REMOVE) $(SRC:.c=.s)
+	$(REMOVE) $(SRC:.c=.d)
+	$(REMOVE) .dep/*
+
+# Include the dependency files.
+-include $(shell mkdir .dep 2>/dev/null) $(wildcard .dep/*)
+
+# Listing of phony targets.
+.PHONY : all begin finish end sizebefore sizeafter gccversion \
+build elf hex eep lss sym coff extcoff \
+clean clean_list program debug gdb-config
---
+++ stk500boot.c
@@ -0,0 +1,591 @@
+/*
+
+Title:     STK500v2 compatible bootloader
+           Heavily based on the work by Peter Fleury
+					 
+Compiler:  avr-gcc 4.8.1 / avr-libc 1.4.3
+Hardware:  Atmega2560/Atmega1284p
+License:   BSD-3-Clause
+
+Modified:  Martyn Brown
+Date:      19 December 2017
+Compiler:  avr-gcc (GCC) 4.8.1
+Description:
+		Heavily modified version of the stk500v2 bootloader used by Arduino,
+		reduced code size and factorised where possible. Tested with latest avrdude
+		to ensure no regression bugs, removed features not required and changed
+		bootloader location and size.
+
+NOTES:
+    Based on Atmel Application Note AVR109 - Self-programming
+    Based on Atmel Application Note AVR068 - STK500v2 Protocol
+*/
+
+#include	<stdint.h>
+#include	<avr/io.h>
+#include	<avr/boot.h>
+#include	<avr/pgmspace.h>
+#include	"command.h"
+
+#ifndef EEWE
+	#define EEWE    1
+#endif
+#ifndef EEMWE
+	#define EEMWE   2
+#endif
+
+/*
+ * define CPU frequency in Mhz here if not defined in Makefile
+ */
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
+
+/*
+ * UART Baudrate, AVRStudio AVRISP only accepts 115200 bps
+ */
+#ifndef BAUDRATE
+	#define BAUDRATE 115200
+#endif
+
+#define BOOT_TIMEOUT 500000   // should be about 1 second
+//#define BOOT_TIMEOUT 3500000 // 7 seconds , approx 2us per step when optimize "s"
+
+/*
+ *  Enable (1) or disable (0) USART double speed operation
+ */
+#ifndef UART_BAUDRATE_DOUBLE_SPEED
+	#define UART_BAUDRATE_DOUBLE_SPEED 1
+#endif
+
+/*
+ * HW and SW version, reported to AVRISP, must match version of AVRStudio
+ */
+#define CONFIG_PARAM_BUILD_NUMBER_LOW		0
+#define CONFIG_PARAM_BUILD_NUMBER_HIGH	0
+#define CONFIG_PARAM_HW_VER							0x0F
+#define CONFIG_PARAM_SW_MAJOR						2
+#define CONFIG_PARAM_SW_MINOR						0x0A
+
+/*
+ * Calculate the address where the bootloader starts from FLASHEND and BOOTSIZE
+ * (adjust BOOTSIZE below and BOOTLOADER_ADDRESS in Makefile if you want to change the size of the bootloader)
+ */
+//#define BOOTSIZE 1024
+/*
+#if FLASHEND > 0x0F000
+	#define BOOTSIZE 8192
+#else
+	#define BOOTSIZE 2048
+#endif
+*/
+
+#define BOOTSIZE 2048
+
+#define APP_END  ( FLASHEND - ( 2 * BOOTSIZE ) + 1 )
+
+/*
+ * Signature bytes are not available in avr-gcc io_xxx.h
+ */
+#if defined (__AVR_ATmega1280__)
+	#define SIGNATURE_BYTES 0x1E9703
+#elif defined (__AVR_ATmega2560__)
+	#define SIGNATURE_BYTES 0x1E9801
+#elif defined (__AVR_ATmega1284P__)
+	#define SIGNATURE_BYTES 0x1E9705
+#else
+	#error "no signature definition for MCU available"
+#endif
+
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega2560__)
+	/* ATMega with two USART, use UART0 */
+	#define	UART_BAUD_RATE_LOW			UBRR0L
+	#define	UART_STATUS_REG					UCSR0A
+	#define	UART_CONTROL_REG				UCSR0B
+	#define	UART_ENABLE_TRANSMITTER	TXEN0
+	#define	UART_ENABLE_RECEIVER		RXEN0
+	#define	UART_TRANSMIT_COMPLETE	TXC0
+	#define	UART_RECEIVE_COMPLETE		RXC0
+	#define	UART_DATA_REG						UDR0
+	#define	UART_DOUBLE_SPEED				U2X0
+#else
+	#error "no UART definition for MCU available"
+#endif
+
+/*
+ * Macro to calculate UBBR from XTAL and baudrate
+ */
+#if UART_BAUDRATE_DOUBLE_SPEED
+	#define UART_BAUD_SELECT(baudRate, xtalCpu) (((float)(xtalCpu))/(((float)(baudRate))*8.0)-1.0+0.5)
+#else
+	#define UART_BAUD_SELECT(baudRate, xtalCpu) (((float)(xtalCpu))/(((float)(baudRate))*16.0)-1.0+0.5)
+#endif
+
+
+/*
+ * States used in the receive state machine
+ */
+#define	ST_START				0
+#define	ST_GET_SEQ_NUM	1
+#define ST_MSG_SIZE_1		2
+#define ST_MSG_SIZE_2		3
+#define ST_GET_TOKEN		4
+#define ST_GET_DATA			5
+#define	ST_GET_CHECK		6
+#define	ST_PROCESS			7
+
+uint8_t	seqNum					=	0;
+
+/*
+ * since this bootloader is not linked against the avr-gcc crt1 functions,
+ * to reduce the code size, we need to provide our own initialization
+ */
+void __jumpMain	(void) __attribute__ ((naked)) __attribute__ ((section (".init9")));
+#include <avr/sfr_defs.h>
+
+//*****************************************************************************
+void __jumpMain(void)
+{
+	asm volatile ( ".set __stack, %0" :: "i" (RAMEND) );
+
+	//	set stack pointer to top of RAM
+	asm volatile ( "ldi	16, %0" :: "i" (RAMEND >> 8) );
+	asm volatile ( "out %0,16" :: "i" (AVR_STACK_POINTER_HI_ADDR) );
+
+	asm volatile ( "ldi	16, %0" :: "i" (RAMEND & 0x0ff) );
+	asm volatile ( "out %0,16" :: "i" (AVR_STACK_POINTER_LO_ADDR) );
+
+	asm volatile ( "clr __zero_reg__" );									// GCC depends on register r1 set to 0
+	asm volatile ( "out %0, __zero_reg__" :: "I" (_SFR_IO_ADDR(SREG)) );	// set SREG to 0
+	asm volatile ( "jmp main");												// jump to main()
+
+}
+
+
+static void readDevice(uint32_t* programAddress, uint16_t msgSize, uint8_t* p)
+{
+	uint16_t data;
+	*p++						=	STATUS_CMD_OK;
+
+	// Read FLASH
+	do {
+#if (FLASHEND > 0x10000)
+		data	=	pgm_read_word_far(*programAddress);
+#else
+		data	=	pgm_read_word_near(*programAddress);
+#endif
+		*p++	=	(uint8_t)data;				// LSB
+		*p++	=	(uint8_t)(data >> 8);	// MSB
+		*programAddress	+=	2;				// Select next word in memory
+		msgSize	-=	2;
+	}
+	while (msgSize);
+}
+
+
+static void programDevice(uint32_t* programAddress, uint32_t* eraseAddress, uint16_t msgSize, uint8_t* buffer)
+{
+	uint32_t tempAddress	=	*programAddress;
+
+	if (*eraseAddress < APP_END )
+	{
+		boot_page_erase(*eraseAddress);
+		boot_spm_busy_wait();
+		*eraseAddress += SPM_PAGESIZE;
+	}
+
+	do {
+		uint16_t word = *buffer++;
+		word += (*buffer++) << 8;
+		boot_page_fill(*programAddress,	word);
+
+		*programAddress	+= 2;
+		msgSize					-=	2;
+	} while (msgSize);
+
+	boot_page_write(tempAddress);
+	boot_spm_busy_wait();
+	boot_rww_enable();				// Re-enable the RWW section
+}
+
+/*
+ * wait for data on USART
+ */
+static int16_t serialAvailable(void)
+{
+
+	return(UART_STATUS_REG & (1 << UART_RECEIVE_COMPLETE));	// wait for data
+
+}
+
+#define	MAX_TIME_COUNT	(F_CPU >> 1)
+
+/*
+ * recieve single byte from USART
+ */
+static uint8_t recieveChar(void)
+{
+
+	while (!(UART_STATUS_REG & (1 << UART_RECEIVE_COMPLETE)))
+		;
+
+	return UART_DATA_REG;
+
+}
+
+/*
+ * transmit single byte to USART, wait until transmission is completed
+ */
+static void transmitChar(char c)
+{
+	
+	UART_DATA_REG	=	c;
+	
+	while (!(UART_STATUS_REG & (1 << UART_TRANSMIT_COMPLETE)))
+		;
+
+	UART_STATUS_REG |= (1 << UART_TRANSMIT_COMPLETE);
+
+}
+
+uint8_t getParameter(uint8_t cmd)
+{
+	uint8_t value;
+
+	switch(cmd)
+	{
+		case PARAM_BUILD_NUMBER_LOW:
+			value	=	CONFIG_PARAM_BUILD_NUMBER_LOW;
+			break;
+		case PARAM_BUILD_NUMBER_HIGH:
+			value	=	CONFIG_PARAM_BUILD_NUMBER_HIGH;
+			break;
+		case PARAM_HW_VER:
+			value	=	CONFIG_PARAM_HW_VER;
+			break;
+		case PARAM_SW_MAJOR:
+			value	=	CONFIG_PARAM_SW_MAJOR;
+			break;
+		case PARAM_SW_MINOR:
+			value	=	CONFIG_PARAM_SW_MINOR;
+			break;
+		default:
+			value	=	0;
+			break;
+	}
+
+	return value;
+}
+
+void recieveData(uint8_t* msgBuffer)
+{
+	uint8_t	checksum				=	0;
+	uint16_t i							=	0;
+	uint16_t msgLength			=	0;
+
+	uint8_t	msgParseState;
+	msgParseState	=	ST_START;
+	while ( msgParseState != ST_PROCESS )
+	{
+		uint8_t c	=	recieveChar();
+		switch (msgParseState)
+		{
+			case ST_START:
+				if ( c == MESSAGE_START )
+				{
+					msgParseState	=	ST_GET_SEQ_NUM;
+					checksum			=	MESSAGE_START ^ 0;
+				}
+				break;
+
+			case ST_GET_SEQ_NUM:
+				seqNum				=	c;
+				msgParseState	=	ST_MSG_SIZE_1;
+				checksum			^=	c;
+				break;
+
+			case ST_MSG_SIZE_1:
+				msgLength			=	c << 8;
+				msgParseState	=	ST_MSG_SIZE_2;
+				checksum			^=	c;
+				break;
+
+			case ST_MSG_SIZE_2:
+				msgLength		|=	c;
+				msgParseState	=	ST_GET_TOKEN;
+				checksum		^=	c;
+				break;
+
+			case ST_GET_TOKEN:
+				if ( c == TOKEN )
+				{
+					msgParseState	=	ST_GET_DATA;
+					checksum			^=	c;
+					i							=	0;
+				}
+				else
+				{
+					msgParseState	=	ST_START;
+				}
+				break;
+
+			case ST_GET_DATA:
+				msgBuffer[i++]	=	c;
+				checksum		^=	c;
+				if (i == msgLength )
+				{
+					msgParseState	=	ST_GET_CHECK;
+				}
+				break;
+
+			case ST_GET_CHECK:
+				if ( c == checksum )
+				{
+					msgParseState	=	ST_PROCESS;
+				}
+				else
+				{
+					msgParseState	=	ST_START;
+				}
+				break;
+			}	//	switch
+		}	//	while(msgParseState)
+}
+
+void appStart( void )
+{
+	uint16_t	data;
+
+#if (FLASHEND > 0x10000)
+	data	=	pgm_read_word_far(0);		//	get the first word of the user program
+#else
+	data	=	pgm_read_word_near(0);	//	get the first word of the user program
+#endif
+
+	if (data != 0xFFFF)							//	make sure its valid before jumping to it.
+	{
+		asm volatile(
+			"clr	r30		\n\t"
+			"clr	r31		\n\t"
+			"ijmp	\n\t"
+		);
+	}
+}
+
+//*****************************************************************************
+int main(void)
+{
+
+	uint8_t *p;
+	uint8_t	msgBuffer[285];
+	uint8_t ispProgram			= 0;
+	uint8_t	checksum				=	0;
+	uint16_t msgLength			=	0;
+	uint32_t address				=	0;
+	uint32_t eraseAddress		=	0;
+	uint32_t bootTimer			= 0;
+
+	uint8_t	mcuStatusReg;
+	mcuStatusReg	=	MCUSR;
+
+	__asm__ __volatile__ ("cli");
+	__asm__ __volatile__ ("wdr");
+	MCUSR		=	0;
+	WDTCSR	|=	( 1 << WDCE ) | ( 1 << WDE );
+	WDTCSR	=	0;
+	__asm__ __volatile__ ("sei");
+	// check if WDT generated the reset, if so, go straight to app
+	if ( mcuStatusReg & ( 1 << WDRF ) )
+	{
+		appStart();
+	}
+
+	/*
+	 * Init UART
+	 * set baudrate and enable USART receiver and transmiter without interrupts
+	 */
+#if UART_BAUDRATE_DOUBLE_SPEED
+	UART_STATUS_REG			|=	( 1 << UART_DOUBLE_SPEED );
+#endif
+	UART_BAUD_RATE_LOW	=	UART_BAUD_SELECT( BAUDRATE, F_CPU );
+	UART_CONTROL_REG		=	( 1 << UART_ENABLE_RECEIVER ) | ( 1 << UART_ENABLE_TRANSMITTER );
+
+	asm volatile ("nop");			// wait until port has changed
+
+	// wait for data
+	while ( (!( serialAvailable() )) && ( ++bootTimer != BOOT_TIMEOUT ) )
+	{
+		asm volatile ("nop");
+	}
+
+	if (bootTimer != BOOT_TIMEOUT)
+	{
+		//	main loop
+		while ( ispProgram == true)
+		{
+			recieveData(msgBuffer);	// Retrieve all the data
+
+			// Now process the STK500 commands, see Atmel Appnote AVR068
+			switch (msgBuffer[0])
+			{
+				case CMD_SIGN_ON:
+					msgLength		=	11;
+					msgBuffer[1] 	=	STATUS_CMD_OK;
+					msgBuffer[2] 	=	8;
+					msgBuffer[3] 	=	'A';
+					msgBuffer[4] 	=	'V';
+					msgBuffer[5] 	=	'R';
+					msgBuffer[6] 	=	'I';
+					msgBuffer[7] 	=	'S';
+					msgBuffer[8] 	=	'P';
+					msgBuffer[9] 	=	'_';
+					msgBuffer[10]	=	'2';
+					break;
+
+				case CMD_GET_PARAMETER:
+					{
+						uint8_t value;
+						value 				= getParameter(msgBuffer[1]);
+						msgLength			=	3;
+						msgBuffer[1]	=	STATUS_CMD_OK;
+						msgBuffer[2]	=	value;
+					}
+					break;
+				case CMD_LEAVE_PROGMODE_ISP:
+					ispProgram		=	false;
+					msgLength			=	2;
+					msgBuffer[1]	=	STATUS_CMD_OK;
+					break;
+				case CMD_SET_PARAMETER:
+				case CMD_ENTER_PROGMODE_ISP:
+					msgLength			=	2;
+					msgBuffer[1]	=	STATUS_CMD_OK;
+					break;
+				case CMD_READ_SIGNATURE_ISP:
+					{
+						uint8_t signatureIndex	=	msgBuffer[4];
+						uint8_t signature;
+
+						if ( signatureIndex == 0 )
+							signature	=	(SIGNATURE_BYTES >> 16) & 0x000000FF;
+						else if ( signatureIndex == 1 )
+							signature	=	(SIGNATURE_BYTES >> 8) & 0x000000FF;
+						else
+							signature	=	SIGNATURE_BYTES & 0x000000FF;
+
+						msgLength			=	4;
+						msgBuffer[1]	=	STATUS_CMD_OK;
+						msgBuffer[2]	=	signature;
+						msgBuffer[3]	=	STATUS_CMD_OK;
+					}
+					break;
+				case CMD_READ_LOCK_ISP:
+					msgLength			=	4;
+					msgBuffer[1]	=	STATUS_CMD_OK;
+					msgBuffer[2]	=	boot_lock_fuse_bits_get( GET_LOCK_BITS );
+					msgBuffer[3]	=	STATUS_CMD_OK;
+					break;
+				case CMD_READ_FUSE_ISP:
+					{
+						uint8_t fuseBits;
+						if ( msgBuffer[2] == 0x50 )
+						{
+							if ( msgBuffer[3] == 0x08 )
+								fuseBits	=	boot_lock_fuse_bits_get( GET_EXTENDED_FUSE_BITS );
+							else
+								fuseBits	=	boot_lock_fuse_bits_get( GET_LOW_FUSE_BITS );
+						}
+						else
+						{
+							fuseBits	=	boot_lock_fuse_bits_get( GET_HIGH_FUSE_BITS );
+						}
+						msgLength			=	4;
+						msgBuffer[1]	=	STATUS_CMD_OK;
+						msgBuffer[2]	=	fuseBits;
+						msgBuffer[3]	=	STATUS_CMD_OK;
+					}
+					break;
+				case CMD_CHIP_ERASE_ISP:
+					eraseAddress		=	0;
+					msgLength				=	2;
+					msgBuffer[1]		=	STATUS_CMD_FAILED;
+					break;
+				case CMD_LOAD_ADDRESS:
+	#if defined(RAMPZ)
+					address	=	( ((uint32_t)(msgBuffer[1]) << 24 ) | ((uint32_t)(msgBuffer[2]) << 16 ) | ((uint32_t)(msgBuffer[3]) << 8 )|(msgBuffer[4]) ) << 1; // convert word to byte address
+	#else
+					address	=	( ((msgBuffer[3]) << 8 ) | (msgBuffer[4]) ) << 1;		// convert word to byte address
+	#endif
+					msgLength			=	2;
+					msgBuffer[1]	=	STATUS_CMD_OK;
+					break;
+				case CMD_PROGRAM_FLASH_ISP:
+					{
+						uint16_t size	=	((msgBuffer[1]) << 8) | msgBuffer[2];
+						programDevice(&address, &eraseAddress, size, msgBuffer + 10);
+						msgLength			=	2;
+						msgBuffer[1]	=	STATUS_CMD_OK;
+					}
+					break;
+				case CMD_READ_FLASH_ISP:
+					{
+						uint16_t	size	=	((msgBuffer[1])<<8) | msgBuffer[2];
+						uint8_t	*p			=	msgBuffer + 1;
+						msgLength				=	size + 3;
+						readDevice(&address, size, p);
+						*p++	=	STATUS_CMD_OK;
+					}
+					break;
+				default:
+					msgLength			=	2;
+					msgBuffer[1]	=	STATUS_CMD_FAILED;
+					break;
+			}
+
+			// Now send answer message back
+			transmitChar(MESSAGE_START);
+			checksum	=	MESSAGE_START ^ 0;
+
+			transmitChar(seqNum);
+			checksum	^=	seqNum;
+
+			uint8_t c	=	((msgLength >> 8 ) & 0xFF);
+			transmitChar(c);
+			checksum	^=	c;
+
+			c			=	msgLength & 0x00FF;
+			transmitChar(c);
+			checksum ^= c;
+
+			transmitChar(TOKEN);
+			checksum ^= TOKEN;
+
+			p	=	msgBuffer;
+			while ( msgLength )
+			{
+				c	=	*p++;
+				transmitChar(c);
+				checksum ^= c;
+				msgLength--;
+			}
+			transmitChar(checksum);
+		}
+	}
+
+	asm volatile ("nop");			// wait until port has changed
+
+	//Now leave bootloader
+	UART_STATUS_REG	&=	0xFD;
+	boot_rww_enable();				// enable application section
+
+	appStart();
+
+	 /*
+	 * Never return to stop GCC to generate exit return code
+	 * Actually we will never reach this point, but the compiler doesn't
+	 * understand this
+	 */
+	for(;;)
+		;
+
+}
